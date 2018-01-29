60c5050c8f70e65aaf8d6e15c037e80ccd6da0b2
Brad Bass <brad.bass@gmail.com>
2018-01-29 15:10:55 -0600

    Sorting action logs descending so newest records always show up first by default

--- src/services/LogService.js
+++ src/services/LogService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default class LogService {
   getAllLogs(successFunc, errorFunc) {
-    axios.get(`${process.env.API_URL}/api/log/action/all`)
+    axios.get(`${process.env.API_URL}/api/log/action/all?sortDirection=DESC`)
       .then(successFunc)
       .catch(errorFunc)
   }
